2c2ce29de61b5a70fad67edd6d73b7d0fc7c0740
Gavin O'Neil <45103746+ONeilGavin@users.noreply.github.com>
2019-10-27 15:43:34 -0400

    Create Client.java

---
+++ Client/Client.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+public class Client implements Runnable {
+	private String name;
+	private BufferedWriter write;
+	private BufferedReader read;
+	private ControlPanel c;
+	
+	public Client(String name, ControlPanel c) {
+		this.name = name;
+		this.c = c;
+		
+		try {
+			Socket clientSocket = new Socket("localhost", 88); //New Socket with server
+			read = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			write = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+			
+		}catch(IOException e) {
+			c.appendToTextArea(e.getMessage());
+		}
+		
+		c.appendToTextArea("Connected to server, chatting as " + "'" + name + "'");
+		
+	}
+	
+	public void sendMessage(String m) {
+		String message = "[" + name + "]: " + m;
+			try {
+				write.write(message); //write message to server
+				write.write("\r\n");
+				write.flush();
+			} catch (IOException e) {
+				c.appendToTextArea(e.getMessage());
+			}
+			
+		
+	}
+	
+	public void run() {
+		try {
+			String message;
+			while((message = read.readLine()) != null) { //read message from server
+				c.appendToTextArea("\n" + message);
+				
+			}
+		}catch(IOException e) {
+			c.appendToTextArea(e.getMessage());
+		}
+		
+	}
+
+}
