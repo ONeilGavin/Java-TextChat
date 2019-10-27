16a25dcffc839fc4287f00a54ac27d891ea37474
Gavin O'Neil <45103746+ONeilGavin@users.noreply.github.com>
2019-10-27 15:45:29 -0400

    Create Server.java

---
+++ Server/Server.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+
+        
+public class Server implements Runnable{
+	private Socket serverSocket;     
+    public static Vector<BufferedWriter> clients = new Vector<BufferedWriter>();
+    private String message;
+    private  JTextArea text;
+    private JScrollPane sPane;
+            
+        public Server(Socket s){
+                 try{
+                	 
+                	System.out.println("Server Started" + "\n");
+                   	serverSocket =s;
+                   	
+                    }catch(Exception e){
+                    	System.out.println(e.getMessage());
+                    }
+                    
+            }     
+        public void run(){
+                   try{
+                	   //Reader and writer for client messages
+                	   BufferedReader reader = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+                       BufferedWriter writer= new BufferedWriter(new OutputStreamWriter(serverSocket.getOutputStream()));
+                            
+                            clients.add(writer); 
+                            
+                        while(true){
+                        	
+                        	message = reader.readLine().trim();
+
+                       System.out.println("\n" + "New Message: " + message);
+                            
+                            for (int i=0;i<clients.size();i++){
+                               try{
+                                    BufferedWriter write = (BufferedWriter)clients.get(i); //Write the message to each client
+                                    write.write(message);
+                                    write.write("\n");
+                                    write .flush();
+                                    
+                                }catch(Exception e){e.printStackTrace();}
+                            }
+                        }  
+                    }catch(Exception e){e.printStackTrace();}
+        }
+            
+        public static void main(String args[]) throws Exception{
+        	
+            ServerSocket sSocket = new ServerSocket(88);
+                while(true){
+                Socket socket = sSocket.accept();
+                Server server=new Server(socket);
+                Thread serverThread=new Thread(server);
+                serverThread.start();
+                }
+            }
+}
